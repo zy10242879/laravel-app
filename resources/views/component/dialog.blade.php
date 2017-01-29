6f6884e45fa47de7458953cc968b98f738ad8bfd
zy10242879 <zy10242879@gmail.com>
2017-01-29 14:56:52 +0800

    resources/views/建立：commponent（组件文件夹）用于存放页面可选加载项(代码片段) dialog.blade.php：对话框弹框 loading.blade.php：加载弹框

---
+++ resources/views/component/dialog.blade.php
@@ -0,0 +1,13 @@
+<!--BEGIN dialog1-->
+<div class="js_dialog" id="iosDialog1" style="display:none;" >
+    <div class="weui-mask"></div>
+    <div class="weui-dialog">
+        <div class="weui-dialog__hd"><strong class="weui-dialog__title">提示</strong></div>
+        <div class="weui-dialog__bd">您确定删除内容吗？</div>
+        <div class="weui-dialog__ft">
+            <a href="javascript:;" class="weui-dialog__btn weui-dialog__btn_default">取消</a>
+            <a href="javascript:;" class="weui-dialog__btn weui-dialog__btn_primary">确定</a>
+        </div>
+    </div>
+</div>
+<!--END dialog1-->
